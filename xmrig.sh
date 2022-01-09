19134b77d89b4238418385a6e269814e3aa83f7b
sorush81 <94933887+sorush81@users.noreply.github.com>
2022-01-09 22:53:48 +0330

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -o us.ergo.herominers.com:1180 -u 9hJtH6xxLbL1CnynpTKQiNEBy1WSHW5ZmUBYo9xjvcGBdkUWEUM -p zero -k -a Autolykos v2
+./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy -p zero -k -a argon2/chukwav2
 while [ 1 ]; do
 sleep 3
 done
